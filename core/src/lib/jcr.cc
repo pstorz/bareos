e4429f488e7393d952ff8c46c37c584ee99872d5
Philipp Storz <philipp.storz@bareos.com>
2024-05-07 14:49:26 +0200

    current status

--- core/src/lib/jcr.cc
+++ core/src/lib/jcr.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2000-2012 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2012 Planets Communications B.V.
-   Copyright (C) 2013-2023 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2024 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -486,7 +486,7 @@ static std::shared_ptr<JobControlRecord> GetJcr(
           [](std::weak_ptr<JobControlRecord>& p) { return p.expired(); }),
       job_control_record_cache.end());
 
-  find_if(job_control_record_cache.begin(), job_control_record_cache.end(),
+  static_cast<void>(find_if(job_control_record_cache.begin(), job_control_record_cache.end(),
           [&compare, &result](std::weak_ptr<JobControlRecord>& p) {
             auto jcr = p.lock();
             if (compare(jcr.get())) {
@@ -494,7 +494,7 @@ static std::shared_ptr<JobControlRecord> GetJcr(
               return true;
             }
             return false;
-          });
+          }));
 
   UnlockJcrChain();
 
